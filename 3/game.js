9e681a66fc4b4287cd08597ae0604bfb21a3bf3e
KaanTanis <kt@kaantanis.com>
2024-07-26 11:00:05 +0300

    wip

---
+++ 3/game.js
@@ -0,0 +1,81 @@
+export default class Game {
+    constructor(gameArea, scoreDisplay, timeDisplay, initScore, initTime) {
+        this.gameArea = gameArea;
+        this.scoreDisplay = scoreDisplay;
+        this.timeDisplay = timeDisplay;
+        this.initScore = initScore;
+        this.initTime = initTime;
+        
+        this.time = this.initTime;
+        this.score = this.initScore;
+
+        this.box = null;
+        this.gameInterval = null;
+        this.highestScore = localStorage.getItem('highestScore') || 0;
+    }
+
+    getRandomPosition() {
+        const x = Math.floor(Math.random() * this.gameArea.clientWidth - 50);
+        const y = Math.floor(Math.random() * this.gameArea.clientHeight - 50);
+
+        return { x, y };
+    }
+
+    createBox() {
+        if (this.box) {
+            this.box.remove();
+        }
+
+        this.box = document.createElement('div');
+        this.box.classList.add('box');
+
+        const { x, y } = this.getRandomPosition();
+        this.box.style.left = `${x}px`;
+        this.box.style.top = `${y}px`;
+
+        this.gameArea.appendChild(this.box);
+
+        this.box.addEventListener('click', this.handleBoxClick.bind(this));
+    }
+
+    handleBoxClick() {
+        this.score++;
+        this.scoreDisplay.textContent = this.score;
+        this.createBox();
+    }
+    
+    startGame() {
+        this.score = this.initScore;
+        this.time = this.initTime;
+
+        this.scoreDisplay.textContent = this.score;
+        this.timeDisplay.textContent = this.time;
+
+        this.createBox();
+        this.gameInterval = setInterval(() => {
+            this.time--;
+            this.timeDisplay.textContent = this.time;
+
+            if (this.time === 0) {
+                clearInterval(this.gameInterval);
+                this.gameOver();
+            }
+        }, 1000);
+    }
+
+    resetGame() {
+        clearInterval(this.gameInterval);
+
+        this.startGame();
+    }
+
+    gameOver() {
+        if (this.score > this.highestScore) {
+            this.highestScore = this.score;
+            localStorage.setItem('highestScore', this.highestScore);
+        }
+
+        alert(`Game Over! Your score: ${this.score}. Highest score: ${this.highestScore}`);
+        this.resetGame();
+    }
+}
---
+++ 3/main.js
@@ -0,0 +1,19 @@
+import Game from './game.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const gameArea = document.querySelector('#gameArea');
+    const scoreDisplay = document.querySelector('#score');
+    const timeDisplay = document.querySelector('#time');
+    const newGameButton = document.querySelector('#newGame');
+
+    const initScore = 0;
+    const initTime = 5;
+
+    const game = new Game(gameArea, scoreDisplay, timeDisplay, initScore, initTime);
+
+    newGameButton.addEventListener('click', () => {
+        game.resetGame();
+    });
+
+    game.startGame();
+});
